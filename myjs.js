8ccb4f21fbea11fdd42c384f9b5b061b6fdb4d41
hungpro2k <83162752+hungpro2k@users.noreply.github.com>
2021-09-14 10:23:59 +0700

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -73,7 +73,7 @@ $('#no').click(() => {
 // generate text in input
 function textGenerate() {
     var n = "";
-    var text = " Tại vì cậu đẹp trai :<< hí hí ";
+    var text = " Tại vì Hưng đẹp trai :v hí hí ";
     var a = Array.from(text);
     var textVal = $('#txtReason').val() ? $('#txtReason').val() : "";
     var count = textVal.length;
